51bdd92adfb50a302a06b41379391d0b33b66a63
Shakib40 <shakibjilani@gmail.com>
2022-02-25 23:48:01 +0530

    Array Done

---
+++ Array/11.Equilibrium_Index.js
@@ -0,0 +1,50 @@
+// INPUT : [-7,1,5,2,-4,3,0]
+// OUTPUT : 3
+
+// function EquilibriumIndex(array){
+//     let left = 0;
+//     let right = array.length - 1;
+//     let mid = 1
+//     let sum_left = 0;
+//     let sum_right = 0;
+
+//     while (  mid < array.length -1 ){
+       
+//         for ( i = 0; i < mid; i++ ){
+//          sum_left += array[i]
+//         }
+
+//         for ( j = mid+1; j < array.length; j++ ){
+//           sum_right += array[j]
+//         }
+
+//         if( sum_left == sum_right ){
+//             return mid
+//         }else{
+//             mid = mid + 1
+//         }
+//     }
+//     return -1
+// }
+
+
+function EquilibriumIndex(array){
+   let sum = 0;
+   let leftsum = 0;
+
+   for(let i = 0; i < array.length; i++){
+       sum += array[i]
+   }
+
+   for(let i = 0; i < array.length; i++){
+       sum -= array[i]
+
+       if(leftsum == sum){
+         return i
+       }
+       leftsum += array[i]
+   }
+   return -1
+}
+
+console.log( EquilibriumIndex( [-7,1,5,2,-4,3,0]  ));
