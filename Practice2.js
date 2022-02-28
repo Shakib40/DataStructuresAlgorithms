f11964fa6648870963ef8905c685537fc9e2d3ac
Shakib40 <shakibjilani@gmail.com>
2022-02-28 13:18:51 +0530

    Update Practice2.js

--- Practice2.js
+++ Practice2.js
@@ -1,35 +1,8 @@
-function MajorityElement(array){
-   
-    
+// let number = [1, 2, 3, 4 , 5]
+// console.log( number.indexOf(5) );
+
+const area = ({ height = 10, width = 20 }) =>{
+  console.log( height * width );
 }
-
-function runProgram(input) {
-    input = input.trim().split("\n");
-    let N = input[0];
-    let array = input[1].trim().split(" ").map(Number);
-    // console.log(N , array ); 
-    console.log( MajorityElement(array) );
-  }
-
-  if (process.env.USERNAME === "shakib") {
-    runProgram(`5
-    3 3 5 5 1`);
-  } else {
-    process.stdin.resume();
-    process.stdin.setEncoding("ascii");
-    let read = "";
-    process.stdin.on("data", function (input) {
-      read += input;
-    });
-    process.stdin.on("end", function () {
-      read = read.replace(/\n$/, "");
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-    });
-    process.on("SIGINT", function () {
-      read = read.replace(/\n$/, "");
-      runProgram(read);
-      process.exit(0) ;
-    });
-  }
-
+// area()
+area({})
