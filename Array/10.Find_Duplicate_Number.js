f045c67b8832e6f4b182ea9067417b3adb492bbc
Shakib40 <shakibjilani@gmail.com>
2022-03-04 21:55:36 +0530

    commit

--- Array/10.Find_Duplicate_Number.js
+++ Array/10.Find_Duplicate_Number.js
@@ -15,3 +15,30 @@
 // Input: nums = [3,1,3,4,2]
 // Output: 3
 
+// const findDuplicates = nums => {
+//     const ans = [];
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[Math.abs(nums[i])-1] < 0)
+//             ans.push(Math.abs(nums[i]));
+//         nums[Math.abs(nums[i])-1] = -1*nums[Math.abs(nums[i])-1];
+//     }
+//     return ans;
+// };
+
+// const findDuplicates = (nums) =>{
+//    let str = "";
+//    for (let i = 0; i < nums.length; i++){
+//       let count = 0
+//       for (let j = 0; j < nums.length; j++){
+//           if(nums[i] == nums[j] && nums[i] != nums[i+1]){
+//               count = count + 1
+//           }
+//       }
+//       console.log(count)
+//    }
+
+// }
+
+// console.log( findDuplicates([1,3,4,2,2,3,1]) );
+
+console.log( findDuplicates([1,1,1,3,3,3,2,2,4,5]) );
